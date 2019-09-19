94f1ae56a4a38377be342b42ce9aa0b6be80a095
maudnals <maud.nalpas@gmail.com>
2019-09-19 08:36:44 +0200

    added promise example

--- async.js
+++ async.js
@@ -1,22 +1,27 @@
 ////////////////////////
 // Callbacks
 ////////////////////////
+
 // The problem is inversion of control (more than callback hell).
 
 ////////////////////////
 // Promises
 ////////////////////////
 
+// --------------------
 // Basic example
+// --------------------
 
-const promise1 = new Promise(function(resolve, reject) {
-  // function(resolve, reject) is called the executor.
-  // 💡💡💡 We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed. That means: we decide on resolve or reject. Makes sense: *we* decide what is defined as success and failure!
-  setTimeout(function() {
-    resolve("foo");
-  }, 300);
+const simplePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo1");
+  }, 100);
 });
-console.log(promise1);
+console.log("simplePromise", simplePromise);
+// function(resolve, reject) is called the executor.
+// 💡💡💡 We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed. That means: we decide on resolve or reject. Makes sense: *we* decide what is defined as success and failure!
+
+// can't "resolve then reject" a promise
 
 // In reality it will look more like:
 // new Promise((resolve, reject) => {
@@ -27,36 +32,44 @@ console.log(promise1);
 //     xhr.send();
 //   });
 
-promise1.then(function(value) {
-  // 💡💡💡 value is whatever we passed in the resolve(...) function above
-  console.log(value);
-  // expected output: "foo"
-});
-//   .catch(error => console.log(error));
+simplePromise
+  .then(
+    value => console.log("value:", value)
+    // 💡 value is whatever we passed in the resolve(...) function above
+    // expected output: "foo"
+  )
+  .catch(error => console.log("error:", error));
 
+// --------------------
 // Promise API:
+// --------------------
 
 // resolve, reject:
-//* Usage 1: see example above when they're referred to from within the executor.
-// Usage 2: create+returns a new promise, that resolves with the given value. Useful to mock the return promise of an async function, or when we want the result of an async function to be thenable.
-// Example for usage 2:
+// Usage 1: see example above when they're referred to from within the executor.
+// Usage 2: "2 in 1 effect" = create+returns a new promise (that resolves with the given value). "Generally, if you don't know if a value is a promise or not, Promise.resolve(value) it instead. And work with the return value as a promise." Useful to create the return value of an async function. For example when the return value depends on conditions, one of them is async and the other is not really, but we want the result of an async function to be THENABLE in both cases.
+// Example:
+
+const promiseCreator = () => Promise.resolve("foo2");
+console.log("promiseCreator", promiseCreator());
+promiseCreator().then(value => console.log("value:", value));
+
 // public async verify(): Promise<boolean> {
 //     if (!this.verifyData()) {
 //      // create+returns a new promise, that resolves with the given value (=false)
-//         Promise.resolve(false);
+//         return Promise.resolve(false);
 //       }
 //       return this.attestation.verify()
 // }
 
-console.log(promise1.resolve);
+// console.log(promise1.resolve);
 
 // race, all
 // catch, then, finally ----- return a new Promise
 // ----
-// create a new promise
-// get the result of a promise
+// how to create a new promise
+// how to get the result of a promise
 
-// / Link between promise and async
+// / Link between promise and async???
 
 // Main promise methods:
 
@@ -112,3 +125,5 @@ console.log(promise1.resolve);
 // https://javascript.info/async-await
 // 999
 // can you do &&?
+
+// main diffs between promise and async aka one is not cancellable
